0cb22683305c6f8ae06953e15324386fc0afff08
peter <peter.roelants@gmail.com>
2015-06-16 15:19:43 +0200

    theano install

--- setup_python.sh
+++ setup_python.sh
@@ -5,6 +5,22 @@
 #  and has most of the libraries you need.
 # More info: https://store.continuum.io/cshop/anaconda/
 
+########################
+# Config and check GPU #
+########################
+
+echo 'Check 3D controller:'
+lspci | grep -i NVIDIA
+
+echo '\nRun nvidia-smi:'
+nvidia-smi
+
+
+
+#########################
+# Install Anaconda base #
+#########################
+echo '\nStart installing Anaconda Python'
 # Get the anaconda install script via 'wget' from the anaconda download page.
 # To install the full anaconda distribution uncomment the next line:
 # wget https://3230d63b5fc54e62148e-c95ac804525aac4b6dba79b00b39d1d3.ssl.cf1.rackcdn.com/Anaconda-2.2.0-Linux-x86_64.sh
@@ -23,20 +39,33 @@ echo 'export PATH=$HOME/miniconda//bin:$PATH'  >> ~/.bashrc
 # Load the update .bashrc file
 source ~/.bashrc
 
+########################################
+# Install Anaconda packages and Theano #
+########################################
+echo '\nStart installing Theano and other packages'
+# use linux 'yes' command to force yes
 # Update anaconda to the latest version
-conda update conda
-conda update anaconda
+yes | conda update conda
 
 # Install Python libraries
-conda install pip  # Python package manager
+yes | conda install pip  # Python package manager
 # Computation deps
-conda install numpy  # Python C vectors and computations
-conda install scipy  # Scientific package
-conda install scikit-learn  # Machine learning package
-conda install pandas  # Data structure and statistics package
+yes | conda install numpy  # Python C vectors and computations
+yes | conda install scipy  # Scientific package
+yes | conda install scikit-learn  # Machine learning package
+yes | conda install pandas  # Data structure and statistics package
 # Interaction and plotting deps
-conda install matplotlib  # Plotting package
-conda install ipython  # Interactive Python sessions
-conda install ipython-notebook  # Python notebooks
+yes | conda install matplotlib  # Plotting package
+yes | conda install ipython  # Interactive Python sessions
+yes | conda install ipython-notebook  # Python notebooks
 # Theano 
-conda install theano  # Theano CPU/GPU computations
+yes | conda install theano  # Theano CPU/GPU computations
+
+###########################
+# Config and check Theano #
+###########################
+echo '\nConfigure and check Theano'
+# Copy theanorc to home folder
+cp -f theanorc $HOME/.theanorc
+# Run theano test
+bash test_theano.sh
